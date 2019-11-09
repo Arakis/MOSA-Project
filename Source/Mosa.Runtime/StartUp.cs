6e82493463bb46ab4dee49e4c85b5c43920606b0
Sebastian Loncar <sebastian.loncar@gmail.com>
2019-11-09 21:44:15 +0100

    added in attribute

--- Source/Mosa.Runtime/StartUp.cs
+++ Source/Mosa.Runtime/StartUp.cs
@@ -2,8 +2,16 @@
 
 namespace Mosa.Runtime
 {
+
+	/// <summary>
+	/// Basic Runtime Initialization Routines
+	/// </summary>
 	public static class StartUp
 	{
+
+		/// <summary>
+		/// This is the main entry point.
+		/// </summary>
 		public static void Initialize()
 		{
 			KernelInitialization();
@@ -13,22 +21,41 @@ namespace Mosa.Runtime
 			StartApplication();
 		}
 
+		/// <summary>
+		/// Setup SSE and other stuff
+		/// This Method is plugged.
+		/// </summary>
 		public static void KernelInitialization()
 		{
 		}
 
+		/// <summary>
+		/// Setup early Boot Memory
+		/// This Method is plugged.
+		/// </summary>
 		public static void SetInitialMemory()
 		{
 		}
 
+		/// <summary>
+		/// Initialize static fields
+		/// This Method is plugged.
+		/// </summary>
 		public static void InitializeAssembly()
 		{
 		}
 
+		/// <summary>
+		/// Initialize the type system.
+		/// This Method is plugged.
+		/// </summary>
 		public static void InitializeRuntimeMetadata()
 		{
 		}
 
+		/// <summary>
+		/// This Method is plugged and will call the .NET Main method
+		/// </summary>
 		public static void StartApplication()
 		{
 		}
